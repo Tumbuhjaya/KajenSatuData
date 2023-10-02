1d877e5020c3d1fe70a3aeff4da6c45dfaa3b1a9
DANANG SR <danangsukoraharjo@gmail.com>
2023-10-02 15:41:48 +0700

    pull server

--- src/views/website.vue
+++ src/views/website.vue
@@ -3,20 +3,20 @@
     <ion-header style="box-shadow: none;">
       <div class="box-header">
         <div class="box-title">
-          <ion-icon :icon="arrowBackCircleOutline" size="large" style="position: absolute;left:0;top:0;bottom: 0;margin:auto;color: #fff;" @click="$router.push('/web_app')"></ion-icon>
+          <ion-icon :icon="arrowBackCircleOutline" size="large" style="position: absolute;left:0;top:0;bottom: 0;margin:auto;color: #1c47bc;" @click="$router.push('/web_app')"></ion-icon>
           <!-- <ion-icon :icon="arrow-back-outline"></ion-icon> -->
-          <h6 style="font-size: 20px;font-weight: bold;color: #fff;">Website</h6>
+          <h6 style="font-size: 20px;font-weight: bold;color: #1c47bc;">Website</h6>
         </div>
       </div>
     </ion-header>
     <ion-content :fullscreen="true" id="page-dashboard">
-      <!-- <div style="width: 100%;height: 40px;background-color: #4c87f2;position: absolute;left:0;right: 0;top:90px;border-bottom-left-radius: 30px;border-bottom-right-radius: 30px;"></div> -->
+      <div style="width: 100%;height: 40px;background-image: url('/assets/13263.jpg'); background-repeat: no-repeat;background-size:cover;background-position: left 50px;position: fixed;left:0;right: 0;top:90px;border-bottom-left-radius: 30px;border-bottom-right-radius: 30px;"></div>
 
-      <ion-img src="/assets/shape-001.png" style="position: fixed;bottom:0;left:0;right:0;"></ion-img>
-      <ion-grid style="padding: 20px !important;">
+      <ion-img src="/assets/shape-new-two.png" style="position: fixed;bottom:0;left:0;right:0;"></ion-img>
+      <ion-grid style="padding: 60px 20px !important;">
         <ion-row  v-for="(item,id) in website" :key="id"  style="margin-top: 5px;margin-bottom: 15px;">
           <ion-col size="12" style="padding: 0;">
-            <div style="width: 100%;box-shadow: 0px 4px 4px 0px #00000040;padding: 20px 15px;border-radius: 8px;background-color: #EFF0F7">
+            <div style="width: 100%;box-shadow: 0px 4px 4px 0px #00000040;padding: 20px 15px;border-radius: 8px;background-color: #eefafd">
               <div style="width: 100%;">
                 <h6 style="font-size: 20px;font-weight: 600;">{{ item.nama }}</h6>
               </div>
@@ -182,39 +182,4 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.box-header{
-  width: 100%;
-  height: ;
-  background-color: #4c87f2;
-  padding: 15px 35px;
-}
-
-.box-header .box-title{
-  width: 100%;
-  height: 70px;
-  background-color: ;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-}
-
-ion-segment-button{
-  background-color: #fff;
-  border-radius: 30px;
-  margin: 0 2.5px 2.5px 2.5px;
-  box-shadow: 0px 2px 4px 0px #00000040;
-}
-
-ion-segment-button label{
-  color: #000;
-}
-
-ion-segment-button::part(indicator-background) {
-  background: transparent;
-}
-ion-icon {
-  font-size: 24px;
-}
-
 </style>
